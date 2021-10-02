c4fc17f8c24eeac72ba8fbc69420841a5f1917f3
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-10-02 21:52:15 +0530

    style: text changes in the modal

--- oauth-fe/src/components/displayFiles/index.js
+++ oauth-fe/src/components/displayFiles/index.js
@@ -33,7 +33,6 @@ const DisplayFiles = () => {
         <div class="modal-body">
             <form>
             <div class="mb-3">
-                <label for="formFileMultiple" class="form-label">Multiple files input example</label>
                 <input class="form-control" type="file" id="formFileMultiple" multiple />
             </div>
             </form>
